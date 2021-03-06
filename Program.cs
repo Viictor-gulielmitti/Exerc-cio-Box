3c8698dea6be9482c50af8cbd3c9fcd1091e257e
Viictor-gulielmitti <79719548+Viictor-gulielmitti@users.noreply.github.com>
2021-03-06 17:57:49 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ExercicioBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("+--------+");
+            Console.WriteLine("| Victor |");
+            Console.WriteLine("+--------+");
+        }
+    }
+}
